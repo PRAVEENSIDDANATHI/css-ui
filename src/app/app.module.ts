f15c8c4dfd954c6ee45af7afabef8b575e0a0028
PRAVEENSIDDANATHI <praveen07081994@gmail.com>
2021-03-08 11:36:57 +0530

    dev -m "customers"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PackagesComponent } from './packages/packages.component';
 import { HomeComponent } from './home/home.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PackagesComponent,
-    HomeComponent
+    HomeComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
